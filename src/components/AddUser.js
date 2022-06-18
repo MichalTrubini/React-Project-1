f472111e7c4f39e1c3f8a222ef1748822839c36f
Michal Trubini <mchl.trubini@gmail.com>
2022-06-18 21:53:05 +0200

    error modal added

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
 import './AddUser.css';
 import Button from './Button';
 import Card from './Card';
+import ErrorModal from './ErrorModal';
 
 const AddUser = (props) => {
 
     const [userName, setUsername] = useState('');
     const [userAge, setUserAge] = useState('');
+    const [error, setError] = useState();
 
     const usernameInputHandler = (event) => {
         setUsername(event.target.value);
@@ -20,8 +23,18 @@ const AddUser = (props) => {
     const submitHandler = (event) => {
         event.preventDefault();
 
-        if (userName.length === 0 || userAge.length === 0) {return;};
-        if (+userAge < 1) {return};
+        if (userName.length === 0 || userAge.length === 0) {
+            setError({
+                title:'Invalid input',
+                message: 'Please enter a valid name or age (non-empty values).'
+            });
+            return};
+        if (+userAge < 1) {
+            setError({
+                title:'Invalid input',
+                message: 'Please enter a valid age (> 0).'
+            });
+            return};
 
         props.OnSubmitData(userName, userAge);
 
@@ -29,15 +42,22 @@ const AddUser = (props) => {
         setUserAge('');
     };
 
-    return <Card className="card-size">
-                <form className='input' onSubmit={submitHandler}>
-                    <label htmlFor='username'>Username</label>
-                    <input id="username" value={userName} type="text" onChange={usernameInputHandler}></input>
-                    <label htmlFor='age'>Age (Years)</label>
-                    <input id="age" type="number" value={userAge} max="120" step="1" onChange={ageInputHandler}></input>
-                    <Button type="submit">Add User</Button>
-                </form>
-        </Card>
+    const errorHandler = () => {
+        setError(null);
+    }
+
+    return <div>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
+            <Card className="card-size">
+                    <form className='input' onSubmit={submitHandler}>
+                        <label htmlFor='username'>Username</label>
+                        <input id="username" value={userName} type="text" onChange={usernameInputHandler}></input>
+                        <label htmlFor='age'>Age (Years)</label>
+                        <input id="age" type="number" value={userAge} max="120" step="1" onChange={ageInputHandler}></input>
+                        <Button type="submit">Add User</Button>
+                    </form>
+            </Card>
+        </div> 
 }
 
 export default AddUser;
--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,14 +1,21 @@
-const ErrorModal = () => {
-    return (
-        <Card>
-            <header>
-                <h2>Error</h2>
-            </header>
-            <p>Something went wrong</p>
-            <footer>
-                
-            </footer>
-        </Card>
+import Button from "./Button";
+import Card from "./Card";
+import './ErrorModal.css'
+
+const ErrorModal = (props) => {
+    return (<div className="backdrop" onClick={props.onConfirm}>
+                <Card className="modal">
+                    <header className="header">
+                        <h2>{props.title}</h2>
+                    </header>
+                    <div>
+                        <p className="content">{props.message}</p>
+                    </div>
+                    <footer className="actions">
+                        <Button type="submit" onClick={props.onConfirm}>OK</Button>
+                    </footer>
+                </Card>
+        </div>
     );
 }
 
